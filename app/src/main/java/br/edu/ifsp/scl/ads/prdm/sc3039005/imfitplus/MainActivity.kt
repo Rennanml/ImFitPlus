0aab3e86b53948b9b7d98a0a47680d82c463f272
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-10-20 16:52:32 -0300

    feat: welcome page

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039005/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039005/imfitplus/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039005.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Boas Vindas"
     }
 }
